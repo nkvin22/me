bc265e90cf9952d1400f418f9fea5a5091177fdd
nkvin22 <nino.kvinikadze.2@btu.edu.ge>
2022-11-30 18:41:10 +0400

    new

--- app/src/main/java/com/example/me/NewFragment2.kt
+++ app/src/main/java/com/example/me/NewFragment2.kt
@@ -2,7 +2,7 @@ package com.example.me
 
 import androidx.fragment.app.Fragment
 
-class NewFragment: Fragment(R.layout.fragment_new) {
+class NewFragment2: Fragment(R.layout.fragment_new) {
 
 
 }
--- app/src/main/java/com/example/me/NotificationFragment.kt
+++ app/src/main/java/com/example/me/NotificationFragment.kt
@@ -3,16 +3,29 @@ package com.example.me
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
 
 class NotificationFragment: Fragment(R.layout.fragment_notification) {
+
+    private lateinit var next: Button
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<TextView>(R.id.textView).text = NotificationFragmentArgs.fromBundle(requireArguments()).name
+
+        next = view.findViewById(R.id.next)
+
+        next.setOnClickListener {
+
+            val action =  NotificationFragmentDirections.actionNotificationFragmentToNewFragment2()
+
+            findNavController().navigate(action)
+        }
 
     }
 
